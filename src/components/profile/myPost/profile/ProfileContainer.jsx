c0f7e62ddcbef4e207a4fe18698e18048cb35907
Ermek Karagulov <ermekkaragulov0@gmail.com>
2023-11-06 16:14:17 +0600

    60

--- src/components/profile/myPost/profile/ProfileContainer.jsx
+++ src/components/profile/myPost/profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import Profile from "./Profile";
 import { connect } from "react-redux";
 import axios from "axios";
 import { setUserProfile } from "../../../redux/profileReducer";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
@@ -23,6 +24,17 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({ profile: state.profilePage.profile });
 
-export default connect(mapStateToProps, {
-  setUserProfile,
-})(ProfileContainer);
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+      let location = useLocation();
+      let navigate = useNavigate();
+      let params = useParams();
+      return (
+          <Component
+              {...props}
+              router={{ location, navigate, params }}
+          />
+      );
+  }
+
+export default connect(mapStateToProps, {setUserProfile,}) (withRouter(ProfileContainer));
